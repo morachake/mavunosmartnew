1da86e1f7c289441cc3cc9fee1f2f1843fcea7a7
morachake <jacobmoracha@gmail.com>
2024-07-12 01:56:32 +0300

    fixing tamplate urls and styles

--- mavuno_smart/urls.py
+++ mavuno_smart/urls.py
@@ -5,6 +5,7 @@ app_name = 'mavuno_smart'
 
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/home/', views.home_view, name='home'),
     path('dashboard/charts/', views.charts_view, name='charts'),
     path('dashboard/tables/', views.tables_view, name='tables'),
 ]
--- mavuno_smart/views.py
+++ mavuno_smart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def home_view(request):
+    return render(request, 'index.html')
 def dashboard_view(request):
     return render(request, 'base.html')
 
